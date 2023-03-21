1256d620f24d03155ac3703a0fc3eb9f2eecd9af
Frank <frankjensen156@gmail.com>
2023-03-21 15:43:28 +0100

    Works but seems wrong

--- server/controller/TaskController.js
+++ server/controller/TaskController.js
@@ -1,72 +1,86 @@
-import Project from '../Models/Mongodb/Project.js';
-import Task from '../Models/Task.js';
-
+import { ObjectId } from "mongodb";
+import Project from "../Models/Mongodb/Project.js";
+import Task from "../Models/Task.js";
 
 export const getTask_list = async (req, res) => {
-  const { projectid } = req.params; 
   try {
-    const tasks = await Project.find({_id: projectid}).select("tasks");
-    
+    const { projectid } = req.params;
+    const tasks = await Project.find({ _id: projectid }).select("tasks");
+
     res.status(200).json(tasks);
-  } catch (error) {
-    res.status(404).json({message: error});
-  }
-}
-
-export const getTask_details = async (req, res) => {
-  try {
-    const { projectid }  = req.params;
-    const { taskid } = req.params;
-    const task = await Project.findOne({_id: projectid}).findById(taskid);
-    
-    res.status(200).json(task);
-  } catch (error) {
-    res.status(404).json({message: error});
-  }
-}
-
-export const postTask = async (req, res) => {
-
-  const data = new Task({
-    worktype: req.body.worktype,
-    title: req.body.title,
-    description: req.body.description,
-    deadline: req.body.deadline,
-    startAt: req.body.startAt,
-    status: req.body.status,
-    users: req.body.users
-  });
-
-  
-  try {
-    // const dataToSave = await data.save();
-    res.status(200).json(dataToSave);
-  } catch (error) {
-    res.status(404).json({message: error.message});
-  }
-}
-
-export const updateTask = async (req, res) => {
-
-  try {
-    const id = req.body._id;
-    const updatedEntity = req.body;
-    const options = { new: true };
-    const result = await Task.findByIdAndUpdate(id, updatedEntity, options);
-
-    res.status(200).send(result);
   } catch (error) {
     res.status(404).json({ message: error });
   }
 };
 
-export const deleteTask = async (req, res) => {
-
+export const getTask_details = async (req, res) => {
   try {
-    const id = req.body._id;
-    const result = await Task.findByIdAndDelete(id);
-    res.status(200).send(`Deleted: ${result.title}`);
+    const { projectid } = req.params;
+    const { taskid } = req.params;
+    const task = await Project.findOne({ _id: projectid }).findById(taskid);
+
+    res.status(200).json(task);
   } catch (error) {
     res.status(404).json({ message: error });
   }
-}
+};
+
+export const postTask = async (req, res) => {
+  try {
+    const { projectid } = req.params;
+    const data = {
+      _id: new ObjectId().toString(),
+      title: req.body.title,
+      workflow: req.body.worktype,
+      description: req.body.description,
+      labels: req.body.labels,
+      users: req.body.users,
+      deadline: new Date().toISOString(),
+      startAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      createdAt: new Date().toISOString(),
+    };
+
+    await Project.findOneAndUpdate({ _id: projectid, $push: { tasks: data } });
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    const { projectid } = req.params;
+    const updatedEntity = {
+      _id: req.body._id,
+      title: req.body.title,
+      workflow: req.body.worktype,
+      description: req.body.description,
+      labels: req.body.labels,
+      users: req.body.users,
+      deadline: req.body.deadline,
+      startAt: req.body.startAt,
+      updatedAt: new Date().toISOString(),
+      createdAt: req.body.createdAt,
+    };
+    const result = await Project.updateOne({_id: projectid, "tasks._id": req.body._id}, {$set: {"tasks.$": updatedEntity}});
+    
+
+    res.status(200).send(updatedEntity);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    const { projectid } = req.params;
+
+    const result = await Project.findOneAndUpdate({_id: projectid, $pull: { tasks: { _id: req.body._id } },});
+
+    res.status(200).send(`Deleted: ${result}`);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
+};
